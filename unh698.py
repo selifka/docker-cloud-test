ce14db7e51bcee846ecfe843f3ed2ce69d5470d6
Selifka <sharayahcorcoran@gmail.com>
2017-04-17 19:22:55 -0400

    Test commit

--- unh698.py
+++ unh698.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 def run_flask():
   return 'UNH698 Website'
+  
+@app.route('/coffee')
+def coffee():
+  return render_template('coffee.html')
 
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0', port=8080)
--- unh698_test.py
+++ unh698_test.py
@@ -21,13 +21,13 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.app.get('/')  
         # Search the page contents for the link to your topic page 
         # Replace xxxxxxxxxxxx with text you'd expect to see on your main page that links to your subpage
-        assert b'Coffee is the best thing in the world' in rv.data 
+        assert b'Coffee Link' in rv.data 
         
     def test_my_topic(self):
         # Replace '/' with the page path you want to make
-        rv = self.app.get('coffee')  
+        rv = self.app.get('/coffee')  
         # Replace UNH698 Website with the text you expect to see on you topic page
-        assert b'UNH698 Website' in rv.data 
+        assert b'Coffee is the best thing in the world' in rv.data 
 
 if __name__ == '__main__':
     unittest.main()
